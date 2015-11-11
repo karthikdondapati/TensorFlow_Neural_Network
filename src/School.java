c4b80338e7e59a1895f802bd2bac46cdf830a3ed
kdondapa <kdondapa@KDONDAPA-1TPAX.cisco.com>
2015-11-11 11:00:58 -0800

    initial commit

---
+++ src/School.java
@@ -0,0 +1,17 @@
+public class School {
+
+	public static void main(String[] args)
+
+	{
+
+		System.out.println("This is sample test class");
+
+	}
+
+	public int getNumber()
+
+	{
+		return 1;
+	}
+
+}
